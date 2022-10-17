7afc141bdef38b11b47c63cb9a1cd35a0b9032c0
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-17 14:42:06 -0300

    useEffect() hook included

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function ClickCounter() {
     const [count, setCount] = useState(0);
+
+    // useEffect(() => {
+    //     document.title = `Page loaded ${count + 1} ${count > 0 ? 'times' : 'time'}`;
+
+    // },[*]);
+    // *variable(s) that when changed call useEffect(). If the array is empty, the function is only called once,
+    // *if the array is not declared as an argument, any rerendering of the component will call the function.
+
+    useEffect(() => {
+        document.title = `Page loaded ${count + 1} ${count > 0 ? 'times' : 'time'}`;
+    });
 
     return (
         <div>
