0c7bafa20a58eabebb3be5e27f5de85b4864e9bf
Vinicius Ramos <ramoscontrole@gmail.com>
2022-10-17 17:19:33 -0300

    conditional rendering outside return()

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 export default function ClickCounter() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(-1);
 
     // useEffect(() => {
     //     document.title = `Page loaded ${count + 1} ${count > 0 ? 'times' : 'time'}`;
@@ -11,23 +11,39 @@ export default function ClickCounter() {
     // *if the array is not declared as an argument, any rerendering of the component will call the function.
 
     useEffect(() => {
-        document.title = `Page loaded ${count + 1} ${count > 0 ? 'times' : 'time'}`;
+        document.title = `Page loaded ${count + 2} ${count > -1 ? 'times' : 'time'}`;
     });
 
-    return (
-        <div>
-            <p>You've clicked {count} times.</p>
+    if (count == -1) {
+        return (
+            <div>
+                <h1>Let's play a super silly game. Yeah, for no reason at all.</h1>
 
-            <button onClick={()=> setCount(count + 1)}>Click me</button>
+                <button onClick={() => setCount(count + 1)} style={{ backgroundColor: 'darkBlue', color: 'white', padding: '5px' }}>Start</button>
 
-            {
-                (count == 0)?
-                <code>Once you begin, you gotta do your best...</code>:
-                (count % 10 == 0 || (count-1) % 10 == 0 && count != 1)?
-                <code>Come on! Is that all you can do!?</code>:
-                <code>Let's goooooo!</code>
-            }
+            </div>
+        );
+    } else if (count < (Math.round(Math.random() * 50 + 50))) {
+        return (
+            <div>
+                <p>You've clicked {count} times.</p>
 
-        </div>
-    );
+                <button onClick={() => setCount(count + 1)}>Click me</button>
+
+                {
+                    (count == 0) ?
+                        <code>Once you begin, you gotta do your best...</code> :
+                        (count % 10 == 0 || (count - 1) % 10 == 0 && count != 1) ?
+                            <code>Come on! Is that all you can do!?</code> :
+                            <code>Let's goooooo!</code>
+                }
+
+            </div>
+        );
+    } else {
+        return (
+            <h1>You won the super silly game. Yeah, for no reason at all.</h1>
+        );
+    }
+
 }
